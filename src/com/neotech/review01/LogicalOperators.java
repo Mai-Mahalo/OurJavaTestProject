36bd2f8389dc2a23e50b743770a5e21106b82677
Mai <mai.y.malanog@gmail.com>
2025-07-04 00:12:12 +0900

    updated a couple of files

--- src/com/neotech/review01/LogicalOperators.java
+++ src/com/neotech/review01/LogicalOperators.java
@@ -3,7 +3,13 @@ package com.neotech.review01;
 import java.util.Scanner;
 
 public class LogicalOperators {
-
+	
+	// If age is from 6 to 12 --> You are a a kid
+	// If age is from 13 to 19 --> You are a teenager
+	// If age >= 20 --> You are a adult
+	
+	
+    // some more info
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
